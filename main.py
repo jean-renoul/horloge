725fa71341201bf25baa7dfebd51ae4b69a1898f
jean-renoul <jean.renoul@laplateforme.io>
2023-12-01 09:20:34 +0100

    ajout des commentaires

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 import datetime
 import time
-  
+
+# Définir les heures initiales pour l'alarme, la pause et l'heure actuelle
 heure = (10, 25, 13)
 alarme = (10, 25, 15)
 pause = (10, 25, 17)
 liste_alarme = list(alarme)
 liste_heure = list(heure)
 liste_pause = list(pause)
+
+# Créer des objets datetime pour les temps d'alarme et de pause
 alarme_datetime = datetime.datetime.now().replace(hour=liste_alarme[0], minute=liste_alarme[1], second=liste_alarme[2])
 pause_datetime = datetime.datetime.now().replace(hour=liste_pause[0], minute=liste_pause[1], second=liste_pause[2])
 
+# Fonction pour gérer la fonctionnalité de pause
 def fonction_pause(x):
     while x == pause_datetime:
-            a = input ("Ecrivez 'unpause' pour mettre fin à la pause : ")
-            if a == "unpause":
-                break
-            else:
-                while True:
-                    (time.sleep(1))
+        a = input("Écrivez 'unpause' pour mettre fin à la pause : ")
+        if a == "unpause":
+            break
+        else:
+            while True:
+                (time.sleep(1))
 
+# Fonction pour gérer la fonctionnalité d'alarme
 def reveil(x):
     if x == alarme_datetime:
         print("ALERTE ALERTE ALERTE")
 
+# Fonction pour formater l'heure en format AM/PM ou 24 heures
 def AM_PM(heure_modifiee, x):
     if x:
         heure_modifiee_syntaxe = heure_modifiee.strftime("%I:%M:%S:%p")
@@ -30,16 +36,22 @@ def AM_PM(heure_modifiee, x):
         heure_modifiee_syntaxe = heure_modifiee.strftime("%H:%M:%S")
     return heure_modifiee_syntaxe
 
+# Fonction pour afficher l'heure actuelle, gérer les alarmes et la pause
 def afficher_heure(x):
     heure_modifiee = datetime.datetime.now().replace(hour=liste_heure[0], minute=liste_heure[1], second=liste_heure[2])
-    
+
     while True:
-        heure_modifiee_syntaxe = AM_PM(heure_modifiee, False)
+        # Formater et afficher l'heure actuelle
+        heure_modifiee_syntaxe = AM_PM(heure_modifiee, True)
         print(heure_modifiee_syntaxe)
         time.sleep(1)
-        
+
+        # Mettre à jour l'heure en ajoutant une seconde
         heure_modifiee = heure_modifiee.replace(second=heure_modifiee.second + 1)
+
+        # Gérer l'alarme et la pause
         reveil(heure_modifiee)
         fonction_pause(heure_modifiee)
 
+# Commencer à afficher l'heure avec l'heure initiale définie
 afficher_heure(heure)
